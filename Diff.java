4b9b8fc4ecbf420540a7cf406d022ede32bf6f63
ntinsley78 <atinsley@carthage.edu>
2014-08-18 21:18:46 -0500

    Looking for similar parts of strings
    We added some code to look for the largest groups of identical strings
    in two strings. We are not there yet but we are getting there.

--- Diff.java
+++ Diff.java
@@ -40,13 +40,30 @@ public class Diff
 
 	public List<DiffChange> getDifferences()
 	{
+		//a list of changes that will turn the source file into the destination file
 		List < DiffChange > retVal = new ArrayList < DiffChange >();
 		
+		//if the two strings have at least one difference
 		if(!getSourceString().equals(getDestinationString()))
 		{
-			
+			//create the differences... how???
+			for(int numCharactersInGroup = getSourceString().length(); numCharactersInGroup > 1;numCharactersInGroup--)
+			{
+				for(int pos = 0;pos <= (getSourceString().length() - numCharactersInGroup);pos++)
+				{
+					String searchString = getSourceString().substring(pos, pos + numCharactersInGroup);
+					if(getDestinationString().indexOf(searchString) >= 0)
+					{
+						System.out.println(searchString);
+					}
+					
+				}
+				System.out.println();
+			}
 		}
+		//else- the two strings are exactly the same, no diff changes
 		
+		//return the differences
 		return retVal;
 	}
 	
--- DiffTest.java
+++ DiffTest.java
@@ -21,7 +21,7 @@ public class DiffTest
 	}
 
 	@Test
-	public void test()
+	public void testSameStrings()
 	{
 		//take two strings and return some kind of list,, 
 		String stringOne = "I have a dog";
@@ -34,5 +34,30 @@ public class DiffTest
 		assertEquals(0, changes.size());
 		
 	}
+	
+	@Test
+	public void test()
+	{
+		//take two strings and return some kind of list,, 
+		String stringOne = "123456789";
+		String stringTwo = "0123mm89";
+		
+		Diff testDiff1 = new Diff(stringOne, stringTwo);
+		
+		List < DiffChange > changes = testDiff1.getDifferences();
+		
+		assertEquals(0, changes.size());
+
+		String stringThree = "I have two wonderful dogs named Willy and Juan Pablo";
+		String stringFour =  "I have two wonderful cats named Willy and Juan Pablo";
+		
+		Diff testDiff2 = new Diff(stringThree, stringFour);
+		
+		changes = testDiff2.getDifferences();
+		
+		assertEquals(0, changes.size());
+
+	}
+	
 
 }
